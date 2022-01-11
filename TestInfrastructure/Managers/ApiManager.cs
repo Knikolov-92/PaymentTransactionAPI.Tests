5e685b0d5b5928640fd8e8ed75c90962b5ed18ad
Kaloyan Nikolov <kanikolov92@gmail.com>
2022-01-11 13:21:33 +0200

    Add ApiManager class

---
+++ TestInfrastructure/Managers/ApiManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+
+namespace PaymentTransactionAPI.Tests.TestInfrastructure.Managers
+{
+    public static class ApiManager
+    {
+        public static string GetBaseUrl()
+        {
+            return ConfigurationManager.AppSettings.Get("BaseUrl");
+        }
+
+        public static string GetRootEndpoint()
+        {
+            return ConfigurationManager.AppSettings.Get("PaymentTransactionsService");
+        }
+
+        public static string GetPaymentTransactionsEndpoint()
+        {
+            return ConfigurationManager.AppSettings.Get("PaymentTransactionsService");
+        }
+
+        public static string GetAdminUserName()
+        {
+            return ConfigurationManager.AppSettings.Get("AdminUserName");
+        }
+
+        public static string GetAdminUserPassword()
+        {
+            return ConfigurationManager.AppSettings.Get("AdminUserPassword");
+        }
+
+        public static string GetAuthorizationHeader()
+        {
+            string credentials = $"{GetAdminUserName()}:{GetAdminUserPassword()}";
+            var byteArray = System.Text.Encoding.UTF8.GetBytes(credentials);
+            string base64Credentials = System.Convert.ToBase64String(byteArray);
+
+            return $"Basic {base64Credentials}";
+        }
+    }
+}
