bfea86b936b5ea27cf64af9e6ee09319fe642be1
Kaloyan Nikolov <kanikolov92@gmail.com>
2022-01-11 13:32:34 +0200

    Implement scenario 1 - approved transaction

--- Steps/PaymentTransactionStepDefinitions.cs
+++ Steps/PaymentTransactionStepDefinitions.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 using PaymentTransactionAPI.Tests.TestInfrastructure.Managers;
+using PaymentTransactionAPI.Tests.TestInfrastructure.Models;
 using RestSharp;
 using TechTalk.SpecFlow;
 
@@ -31,7 +34,42 @@ namespace PaymentTransactionAPI.Tests.Steps
         [Then("^on POST request with valid payment transaction to /payment_transactions status code 200 is returned$")]
         public void ThenOnPostRequestWithValidPaymentTransaction_StatusCode200IsReturned()
         {
-           
+            _request = new RestRequest(ApiManager.GetPaymentTransactionsEndpoint(), Method.POST);
+            _request.AddHeader("Content-Type", "application/json;charset=UTF-8");
+            _request.AddHeader("Authorization", ApiManager.GetAuthorizationHeader());
+
+            var transaction = new PaymentTransaction
+            {
+                PaymentTransactionObject = new PaymentTransactionBody()
+                {
+                    CardNumber = "4200000000000000",
+                    Cvv = "123",
+                    ExpirationDate = "01/2023",
+                    Amount = "1000",
+                    Usage = "Tax",
+                    TransactionType = "sale",
+                    CardHolder = "Ivancho",
+                    Email = "ivancho@isthebest.com",
+                    Address = "Ivanolandia, Ivanov street 123"
+                }
+            };
+            var body = JsonConvert.SerializeObject(transaction);
+
+            _request.AddJsonBody(body);
+            _response = _client.Execute(_request);
+
+            var json = JObject.Parse(_response.Content);
+            string refNumber = json.GetValue("unique_id").ToString().Trim();
+            string status = json.GetValue("status").ToString().Trim();
+            string message = json.GetValue("message").ToString().Trim();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(_response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+                Assert.That(refNumber.Length, Is.EqualTo(32));
+                Assert.That(status, Is.EqualTo("approved"));
+                Assert.That(message, Is.EqualTo("Your transaction has been approved."));
+            });
         }
 
         [Then("^on POST request with valid void transaction to /payment_transactions status code 200 is returned$")]
